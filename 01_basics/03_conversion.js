a6d711cfa7a1392bad2ff416d26471d7ddf4f584
kaptan danuj <kaptandanuj@gmail.com>
2026-03-12 10:22:59 +0000

    Hello Boi

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -49,3 +49,36 @@ console.log(undefined + 1); // NaN => Not-a-Number
 console.log(true + true);   // 2
 console.log(false + true);     // 1
 console.log(false + 1);     //1
+
+// industry unfriendly
+console.log("1"+2+2); //122
+console.log(3+2+"1"); //51
+console.log(3+"2"+1); //321
+console.log(+true); // 1
+console.log(+""); //0
+
+//let num1, num2, num3
+//num1 = num2 = num3 = 2 + 2
+
+// let Score =400;
+// Score++;
+// console.log(Score); //401
+// ++Score
+// console.log(Score); //401
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3" because x++ first takes value then increment thats why y=3
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4" because ++a first increments then takes value or store thats why b=4 and a =4
+
+// extra
+console.log(0 == false)   // true
+console.log(0 === false)  // false because === is strict and want same type
+console.log("" == false)  // true
